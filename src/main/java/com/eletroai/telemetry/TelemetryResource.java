e7d1b88057bfb9b6261d1d016fe16e41dfcaa4b4
luisalmeida <luis.m.almeida0@gmail.com>
2026-06-19 11:39:15 +0100

    Add Controller to receive data from iot devices and add them to redpanda kafka message Queue

---
+++ src/main/java/com/eletroai/telemetry/TelemetryResource.java
@@ -0,0 +1,29 @@
+package com.eletroai.telemetry;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+@Path("/telemetry")               // 1. Define a rota base do "Controller"
+@ApplicationScoped               // 2. Define o ciclo de vida no Quarkus
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class TelemetryResource {
+
+    @Inject
+    @Channel("eletroai-metrics")
+    Emitter<TelemetryRecord> telemetryEmitter;
+
+    @POST
+    public Response receiveMetrics(TelemetryRecord payload) {
+        telemetryEmitter.send(payload);
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+}
